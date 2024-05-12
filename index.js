e452b22c73eeed76b671f5a196d2badda4d77eab
Anik98B <anikbhowmik0188@gmail.com>
2024-05-12 14:49:05 +0600

    create jwt token

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
@@ -30,6 +31,16 @@ async function run() {
         const queriesCollection = client.db('alternativeStocks').collection('queries');
         // const userCollection = client.db('alternativeStocks').collection('user');
 
+        //auth related api
+        app.post('/jwt', async (req, res) => {
+            const user = req.body;
+            console.log(user);
+            const token= jwt.sign(user, 'secret', {expiresIn: '1h'})
+            res.send(token);
+        })
+
+
+        //queries related api
         app.get('/queries', async (req, res) => {
             const cursor = queriesCollection.find();
             const result = await cursor.toArray();
